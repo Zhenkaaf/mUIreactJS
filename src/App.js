bad8fc50576136bff294c3898830f7193ec93bf6
Zhenkaaf <zhenkaaf@gmail.com>
2024-01-03 13:24:06 +0200

    themeProvider-styledComponents-sx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { Add, Settings } from "@mui/icons-material";
+import { Button, Typography, styled } from "@mui/material";
 
 function App() {
+  const BlueButton = styled(Button)(({ theme }) => ({
+    backgroundColor: theme.palette.otherColor.main,
+    color: "#888",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightcoral",
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white",
+    },
+  }));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="text">Text</Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        startIcon={<Settings />}
+      >
+        Settings
+      </Button>
+      <Button
+        variant="contained"
+        color="success"
+        startIcon={<Add />}
+      >
+        Add new post
+      </Button>
+      <Button
+        variant="outlined"
+        disabled
+        size="small"
+      >
+        Outlined
+      </Button>
+      <Typography
+        variant="h3"
+        component="p"
+      >
+        p tag here! hello from typography
+      </Typography>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "skyblue",
+          color: "#888",
+          margin: 5,
+          "&:hover": {
+            backgroundColor: "lightcoral",
+          },
+          "&:disabled": {
+            backgroundColor: "gray",
+            color: "white",
+          },
+        }}
+      >
+        My unique button
+      </Button>
+      <BlueButton>Other button</BlueButton>
     </div>
   );
 }
